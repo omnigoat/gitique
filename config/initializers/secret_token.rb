7106b0172c29506536f19422230e8a554166e8ea
jona <omnigoat@gmail.com>
2010-10-06 16:37:51 +1100

    the beginning of gitique

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitique::Application.config.secret_token = 'aebe40ffc1113466ba79d9427baf54bc37f3d1b5707be4921ac6c9a60017634ad723b0e643f3ba454f1cb24c7da0e30420ddf10d4fbf0cf9c5cb72fa17edb01d'
