033d8ad5227516bef6121b5988c2e3fbbd9e91b3
jona <omnigoat@gmail.com>
2010-12-07 12:16:49 +1100

    blah

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -15,20 +15,19 @@ class RepositoriesController < ApplicationController
       repo = Repository.new(:url => params[:url])
       repo.save
       
-      k = "git clone --bare " + params[:url] + " repo" + repo.id.to_s + ".git"
+      # this is a little hacky, but I need it to work both on my development machine
+      # and my production machine
+      k = "cd `pwd -P` && " + IO.popen('which git').readlines[0].chomp! + " clone --bare " + params[:url] + " resources/repositories/r" + repo.id.to_s + ".git"
       logger.info "{{" + k + "}}"
       
       Open3.popen3(k) do |stdin, stdout, stderr|
         logger.info "STDOUT[" + stdout.readlines.join + "]"
         logger.info "STDERR[" + stderr.readlines.join + "]"
       end
-      #system k
       
-      
-      
-      #logger.info fd.readlines.join("")
     else
       logger.info "can't add repo" + repo.id.to_s + " - it already exists!"
+      
     end
     
     render :nothing => true
@@ -41,7 +40,7 @@ class RepositoriesController < ApplicationController
     repo = Repository.find_by_url(params[:url])
     return if not repo
       
-    k = "rm -rf resources/repositories/repo" + repo.id.to_s
+    k = "rm -rf resources/repositories/r" + repo.id.to_s + ".git"
     #logger.debug k
     IO.popen(k)
     
