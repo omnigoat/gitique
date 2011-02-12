4f77a2787e91c7006e6b34f2f5bdb464d41d2247
jona <omnigoat@gmail.com>
2011-02-13 04:49:42 +1100

    correction - NOW we have working tests for the user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
 	def main
-		@k = User.new(:username => "hooray!", :email => "thing@thing.com")
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
+    @attr = { :username => "Example User", :email => "user@example.com" }
   end
 
   it "should create a new instance given valid attributes" do
     User.create!(@attr)
   end
 
-  it "should require a name"
+  it "should require a name" do
+  	no_name_user = User.new(@attr.merge(:username => ""))
+    no_name_user.should_not be_valid
+  end
+
 end
