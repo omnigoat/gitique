7c5001c5fe20d2f58184153eae22052cf65987a8
jona <omnigoat@gmail.com>
2011-02-13 05:09:51 +1100

    more user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ActiveRecord::Base
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-	validates :username, :presence => true,
-	                     :length   => { :maximum => 32 }
-	validates :email,    :presence => true,
-	                     :format   => { :with => email_regex }
+	validates :username, :presence   => true,
+	                     :length     => { :maximum => 32 },
+	                     :uniqueness => { :case_sensitive => false }
+	validates :email,    :presence   => true,
+	                     :format     => { :with => email_regex },
+	                     :uniqueness => { :case_sensitive => false }
 end
