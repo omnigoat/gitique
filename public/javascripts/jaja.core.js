fa1123430bc4a541e06bb591969c0fc7c74e0b28
jona <omnigoat@gmail.com>
2011-02-18 19:58:24 +1100

    - more work on UI, to do with switching tabs

--- public/javascripts/jaja.core.js
+++ public/javascripts/jaja.core.js
@@ -2,6 +2,15 @@
 
 ;(function(jaja, $, undefined) {
   
+	// Array Remove - By John Resig (MIT Licensed)
+	Array.prototype.remove = function(from, to) {
+		var rest = this.slice((to || from) + 1 || this.length);
+		this.length = from < 0 ? this.length + from : from;
+		return this.push.apply(this, rest);
+	};
+
+	
+
   $.extend(jaja, {
 	fold: function(xs, f, init) {
 		for (var i = 0, z = xs.length; i != z; ++i) {
